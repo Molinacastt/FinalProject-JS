5bedd2454e59969a5e0ca6899b98e1be9cb95960
Alejandro Molina <molina_castt@hotmail.com>
2023-07-27 18:07:10 -0500

    Final project - JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* LOGIN */ 
+
+document.getElementById("form-group").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username !== "" && password !== "") {
+        window.location.href = "../pages/producto.html";  
+    } else {
+        alert("Por favor, complete todos los campos del formulario.");
+    }
+});
